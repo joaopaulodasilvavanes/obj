20885a6ca574a642fc7a23208a37d4beaaaf2e8b
joao paulo <142436189+joaopaulodasilvavanes@users.noreply.github.com>
2023-09-04 17:26:23 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,72 @@
+*let filme =  {
+    nome  velozesefuriososx,
+    lançamento 2023,
+elenco: ["vindiesel","jasonmomoa"],
+diretor: ["louisleterrier"],
+assistido: true
+
+}
+
+
+console.log(lançamento);
+console.log(elenco);
+console.log(diretor);
+console.log(filmeassistido);
+*/
+
+/*
+let pessoa = {
+nome: "joao",
+idade: 16,
+generomusical: "rap",
+}
+/console.log("nome da pessoa é" ,pessoa.nome + "ela tem" , pessoa.idade + "anos e gosta muito de",pessoa.generomusical);
+*/
+
+/*
+let filme =  {
+    nome:  'velozesefuriososx',
+    lançamento: 2023,
+elenco: ["vindiesel","jasonmomoa"],
+diretor: ["louisleterrier"],
+assistido: true
+}
+filme.personagens = ['vindiesel','jasonmomoa',]
+console.log(filme.elenco[0] +" fez"+filme.personagens );
+console.log(filme.elenco[1] +"fez "+filme.personagens);
+console.log(filme.elenco[2] + " fez"+ filme.personagens);
+
+filme.elenco[0] = 'xuxa';
+console.log(filme.elenco[0]);
+console.log(filme);
+*/
+/*
+const usuario = {
+    nome:  'prof',
+    idade: 25,
+email: 'prof@senacrs.com.br',
+cidade: 'sao paulo'
+}
+const novoUsuario = {
+    ...usuario,
+    nome: 'joao',
+    idade: 28
+}
+console.log(usuario);
+console.log(novoUsuario);
+*/
+/*const listaDeNomes = ["mika", "paula", "vitor"]
+const copialistaDeNomes = [...listaDeNomes]
+console.log(copiaListaDeNomes) //["mika", "Paula", "vitor"]
+*/
+/*
+let pessoa = {
+nome: 'joao paulo'
+idade: 16,
+generomusical 'rap',
+}
+function lerobjetopessoas(objeto){
+let novoobjeto = (...objeto)
+novoobjeto.objeto.comidaspreferidas = ['lasanha' 'pizza'];
+novoobjeto
+}
